45449a5ba204f25de5213de061c221485d85855c
Asfar Khan <asferk8@gmail.com>
2026-06-30 16:01:39 +0530

    3rd Commit

---
+++ my-Portfolio/src/App.jsx
@@ -0,0 +1,14 @@
+export default function App(){
+    return (
+        <>
+            <Navbar />
+            <main>
+                <Hero />
+                <About />
+                <Projects />
+                <Skills />
+                <Contact />
+            </main>
+        </>
+    );
+}
