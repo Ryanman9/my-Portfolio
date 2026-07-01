bd71c586b0fad0af75002162c6a84d97664afb19
Asfar Khan <asferk8@gmail.com>
2026-07-01 12:52:53 +0530

    10th Commit

--- my-Portfolio/src/App.jsx
+++ my-Portfolio/src/App.jsx
@@ -1,3 +1,10 @@
+import Navbar from "./components/Navbar/Navbar";
+import Hero from "./sections/Hero/Hero";
+import About from "./sections/About/About";
+import Projects from "./sections/Projects/Projects";
+import Skills from "./sections/Skills/Skills";
+import Contact from "./sections/Contact/Contact";
+
 export default function App(){
     return (
         <>
