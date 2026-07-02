bf2523f4e0da3765ac9c226641892c4548790d3f
Asfar Khan <asferk8@gmail.com>
2026-07-02 13:19:34 +0530

    14th Commit

--- my-Portfolio/src/App.jsx
+++ my-Portfolio/src/App.jsx
@@ -4,6 +4,7 @@ import About from "./sections/About/About";
 import Projects from "./sections/Projects/Projects";
 import Skills from "./sections/Skills/Skills";
 import Contact from "./sections/Contact/Contact";
+import "./styles/global.css";
 
 export default function App(){
     return (
